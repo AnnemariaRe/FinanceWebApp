3e89f61cfe3e7ddf436355d1927fc7f66bc7996c
Annemaria <annemariarepenko@gmail.com>
2022-09-15 20:39:29 +0300

    add: style.css

--- src/components/header.js
+++ src/components/header.js
@@ -7,15 +7,12 @@ class Header extends HTMLElement {
       this.innerHTML = `
         <header>
         <div class="menu-bar">
-            <img src="src/img/budget_icon.png" width="80px" height="80px"/>
-            <h1>Finance app</h1>
+            <img src="src/img/budget_icon.png" width="120px" height="120px"/>
             <nav class="menu-nav">
-                <ul class="menu-list">
-                    <li><a href="index.html">Главная</a></li>
-                    <li><a href="src/pages/wallet.html">Кошелек</a></li>
-                    <li><a href="src/pages/history.html">История</a></li>
-                    <li><a href="src/pages/settings.html">Настройки</a></li>
-                </ul>
+                <a href="index.html">Главная</a>
+                <a href="src/pages/wallet.html">Кошелек</a>
+                <a href="src/pages/history.html">История</a>
+                <a href="src/pages/settings.html">Настройки</a>
             </nav>
         </div>
         </header>
