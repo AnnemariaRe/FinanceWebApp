ece17b0ae718adc0c75d605bef8d6bafd37d2913
AnnemariaRe <annemariarepenko@gmail.com>
2022-10-02 19:14:44 +0300

    fix: page-load.js

--- src/components/page-load.js
+++ src/components/page-load.js
@@ -1,9 +1,7 @@
 var before = new Date().getTime();  
-    window.onload = Pageloadtime;
+window.onload = Pageloadtime;
 
-    function Pageloadtime() {  
-        var after = new Date().getTime();  
-        pageloadtime = (after - before) / 1000  
-    
-        document.getElementById("load-time").innerHTML = "Page load time: " + pageloadtime + " sec";  
-    }  
+function Pageloadtime() {  
+    var after = new Date().getTime();  
+    document.getElementById("load-time").innerHTML = "Page load time: " + (after - before) / 1000  + " sec";  
+}  
