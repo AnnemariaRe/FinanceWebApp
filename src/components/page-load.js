dad019c6c0209367d956559770ac2512254196f8
AnnemariaRe <annemariarepenko@gmail.com>
2022-10-28 16:11:15 +0300

    fix: main page

--- src/components/page-load.js
+++ src/components/page-load.js
@@ -3,5 +3,5 @@ window.onload = Pageloadtime;
 
 function Pageloadtime() {  
     var after = new Date().getTime();  
-    document.getElementById("load-time").innerHTML = "Page load time: " + (after - before) / 1000  + " sec";  
+    console.log("Page load time: " + (after - before) / 1000  + " sec");
 }  
